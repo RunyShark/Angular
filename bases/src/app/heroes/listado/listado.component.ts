837230de0b88cc3eb070c468452f6c6987e8427b
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-21 17:36:59 -0500

    fors

--- bases/src/app/heroes/listado/listado.component.ts
+++ bases/src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'ironman', 'Hulk', 'Capitan America'];
 }
