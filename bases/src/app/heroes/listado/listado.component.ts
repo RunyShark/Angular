a7ade8f3ad624c9b719a4ebf89a6b6b3dd26a1ab
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-21 17:39:16 -0500

    fors

--- bases/src/app/heroes/listado/listado.component.ts
+++ bases/src/app/heroes/listado/listado.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
   heroes: string[] = ['Spiderman', 'ironman', 'Hulk', 'Capitan America'];
+  deleteList(): void {
+    this.heroes = [];
+  }
 }
