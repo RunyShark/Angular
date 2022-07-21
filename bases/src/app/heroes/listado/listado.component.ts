7cc51d2628d05bf7ec8e20bbe118a5d0ff2aeff4
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-21 17:51:25 -0500

    fors

--- bases/src/app/heroes/listado/listado.component.ts
+++ bases/src/app/heroes/listado/listado.component.ts
@@ -8,7 +8,6 @@ export class ListadoComponent {
   heroes: string[] = ['Spiderman', 'ironman', 'Hulk', 'Capitan America'];
   saveHero: string | undefined = '';
   deleteList(): void {
-    const save = this.heroes.pop();
-    this.saveHero = save;
+    this.saveHero = this.heroes.shift() || '';
   }
 }
