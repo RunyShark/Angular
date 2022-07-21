94e20a8ce5e0aa542ec642c48cc8ead7bb8d75e2
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-21 17:30:16 -0500

    heros

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HeroeComponent, ContadorComonent, AppComponent } from './index';
+import {
+  HeroeComponent,
+  ContadorComonent,
+  AppComponent,
+  ListadoComponent,
+} from './index';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComonent, HeroeComponent],
+  declarations: [
+    AppComponent,
+    ContadorComonent,
+    HeroeComponent,
+    ListadoComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- bases/src/app/heroes/index.ts
+++ bases/src/app/heroes/index.ts
@@ -1,1 +1,2 @@
 export * from './heroe/heroes.component';
+export * from './listado/listado.component';
