dcb3d447e71c1d41ef1c0d3063fa18472315b360
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-22 07:40:19 -0500

    Froms

--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+interface Character {
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  characters: string[] = ['Goku', 'Krilim'];
+  character: Character[] = [
+    {
+      name: 'Goku',
+      power: 10,
+    },
+    {
+      name: 'Gjan',
+      power: 101,
+    },
+  ];
+  newCharacter: Character = {
+    name: '',
+    power: 0,
+  };
 
-  addNewCharacter() {}
+  addNewCharacter() {
+    if (this.newCharacter.name.trim().length === 0) return;
+    this.character.push(this.newCharacter);
+    this.newCharacter = { name: '', power: 0 };
+  }
 
+  onChange(target: any) {}
   ngOnInit(): void {}
 }
